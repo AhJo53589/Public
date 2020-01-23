537c6799cdb8235cce1a8eb1ae7684a2ddc3fda5
Ikaruga <ahjo_bb@hotmail.com>
2020-01-23 20:21:13 +0800

    update

--- Lua/Workspace/hello.lua
+++ Lua/Workspace/hello.lua
@@ -2,7 +2,7 @@ print("Hello AhJo")
 b = 100
 print(type(b))
 print(b)
-
+print(#(b .. ""))
 
 a = {}
 a["key"] = "value"
@@ -12,3 +12,15 @@ a[key] = a[key] + 11
 for k, v in pairs(a) do
     print(k .. " : " .. v)
 end
+
+print(c)
+c = 10
+print(c)
+
+print(type(print))
+print("10" + 1)
+print(12 .. 13)
+print(20 .. "" == "20")
+print((nil or 12) and 13)
+print(12 and nil)
+
